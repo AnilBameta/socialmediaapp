3ab03a411c8a3b3188df1462fee8dbe11f654115
AnilBameta <anilbameta@gmail.com>
2022-01-25 19:19:20 +0530

    Initialize project using Create React App

---
+++ Backend/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const {ApolloServer,gql} = require('apollo-server-express');
+const mongoose = require('mongoose')
+const resolvers = require('./Resolvers');
+const typeDefs = require('./Typedefs');
+
+
+
+async function startServer(){
+    const app=express()
+    const apolloServer=new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+    await apolloServer.start()
+   
+    apolloServer.applyMiddleware({app:app});
+   
+    app.use((req,res)=>{
+        res.send("Hello From Apollo Server");
+    })
+   mongoose.connect('mongodb+srv://AnilBameta:AnilgotAtlas@cluster0.9zgnr.mongodb.net/SMData?retryWrites=true&w=majority')
+   .then(()=>console.log("Database Connected"))
+   .catch(err=> console.log(err))
+    app.listen(5000,()=>console.log("Apollo Server Is Running"))
+   }
+   startServer();
+
+
